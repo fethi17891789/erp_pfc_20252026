753fdf1dcb034b6886e1f6034e77280e3fb271e8
FETHI MESLI <fethi.mesli@univ-tlemcen.dz>
2025-12-30 19:17:41 +0100

    front end création produits

---
+++ wwwroot/js/ProduitNew.js
@@ -0,0 +1,16 @@
+﻿// wwwroot/js/ProduitNew.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var fileInput = document.getElementById("ProductImage");
+    var fileNameSpan = document.getElementById("ProductImageName");
+
+    if (fileInput && fileNameSpan) {
+        fileInput.addEventListener("change", function () {
+            if (this.files && this.files.length > 0) {
+                fileNameSpan.textContent = this.files[0].name;
+            } else {
+                fileNameSpan.textContent = "";
+            }
+        });
+    }
+});
