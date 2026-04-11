6a5fbacee564e56dfe4781944aeca4edb6dcb109
FETHI MESLI <fethi.mesli@univ-tlemcen.dz>
2026-04-11 16:16:51 +0100

    UI UX module annuaire

---
+++ Presentation/view/AnnuaireList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace erp_pfc_20252026.Pages
+{
+    public class AnnuaireListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
---
+++ Presentation/view/AnnuaireNew.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace erp_pfc_20252026.Pages
+{
+    public class AnnuaireNewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
