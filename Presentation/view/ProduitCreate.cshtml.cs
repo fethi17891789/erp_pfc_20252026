2b7c356a7b45ff7e1edebc0db4237f6763df43bb
FETHI MESLI <fethi.mesli@univ-tlemcen.dz>
2026-04-23 18:05:04 +0100

    correction namespace

--- Presentation/view/ProduitCreate.cshtml.cs
+++ Presentation/view/ProduitCreate.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace erppfc20252026.Pages
+namespace erp_pfc_20252026.Pages
 {
     public class ProduitsModel : PageModel
     {
