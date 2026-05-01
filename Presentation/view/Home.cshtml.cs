4caee294c70f5d85639e9883013ebaf4de537068
FETHI MESLI <fethi.mesli@univ-tlemcen.dz>
2026-05-01 15:18:14 +0100

    màj écran d'accueil

--- Presentation/view/Home.cshtml.cs
+++ Presentation/view/Home.cshtml.cs
@@ -1,15 +1,32 @@
 // Fichier : Presentation/view/Home.cshtml.cs
-using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Donnees;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace erp_pfc_20252026.Pages
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        private readonly ErpDbContext _context;
+
+        public HomeModel(ErpDbContext context)
         {
-            // Pour l�instant aucune logique : simple �cran d�accueil factice.
+            _context = context;
         }
 
+        public int NbProduits { get; set; }
+        public int NbContacts { get; set; }
+        public int NbVehiculesEnRoute { get; set; }
+        public int NbVehiculesTotal { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            NbProduits = await _context.Produits.CountAsync();
+            NbContacts = await _context.Contacts.CountAsync();
+            NbVehiculesTotal = await _context.LogistiqueVehicules.CountAsync();
+            NbVehiculesEnRoute = await _context.LogistiqueVehicules
+                .CountAsync(v => v.Statut == "En Trajet");
+        }
     }
 }
